4b2d9f6bc8f851bd9c2315220a0e96a3745e4fff
Albert <aeskenazi@gmail.com>
2014-01-19 20:46:31 -0500

    Prepend any output from the JS app with "[JS App] " to make it clear in the console which output is coming from the Pebble app and which is coming from the JS app.

--- js_server/header.js
+++ js_server/header.js
@@ -11,6 +11,17 @@ var localStorage = Pebble.localStorage;
 
 var XMLHttpRequest = require('./simdata/XMLHttpRequest').XMLHttpRequest;
 
+console.log = ((function() {
+    var orig_log = console.log;
+    return function() {
+        if ((arguments.length > 0) &&
+            (typeof(arguments[0]) === 'string'))
+            arguments[0] = "[JS App] " + arguments[0];
+
+        orig_log.apply(console, arguments);
+    };
+})());
+
 /////////////////////////////////////////////////
 
 
